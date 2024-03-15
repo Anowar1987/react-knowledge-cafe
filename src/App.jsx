5fa7e8baf3b1c07819e237383eb75217a28d6221
Anowar1987 <anowarhossen.iim@gmail.com>
2024-03-16 01:30:08 +0600

    mookmark added

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,20 @@ import Header from './Component/Header/Header'
 import Blogs from './Component/Blogs/Blogs'
 import Bookmarks from './Component/Bookmarks/Bookmarks'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [bookmarks, setBookmarks] = useState([])
+  const handleAddBookmark = blog => {
+    const newBookmarks = [...bookmarks, blog];
+    setBookmarks(newBookmarks);
+  }
 
   return (
     <>
       <Header></Header>
-      <div className='md:flex'>
-      <Blogs></Blogs>
-      <Bookmarks></Bookmarks>
+      <div className='md:flex max-w-7xl mx-auto'>
+      <Blogs handleAddBookmark = {handleAddBookmark}></Blogs>
+      <Bookmarks bookmarks = {bookmarks}></Bookmarks>
       </div>
     </>
   )
--- src/Component/Blog/Blog.jsx
+++ src/Component/Blog/Blog.jsx
@@ -1,14 +1,40 @@
 import PropTypes from 'prop-types';
-const Blog = (blog) => {
-    console.log(blog);
+import { FaRegBookmark } from "react-icons/fa";
+
+const Blog = ({blog, handleAddBookmark}) => {
+    const {title, cover_img, author_img, reading_time, author, posted_date, hashtag} = blog;
     return (
         <div>
-            
-        </div>
+            <img className='w-full mb-10' src={cover_img} alt={`cover picture of the title ${title}`} />
+            <div className='flex justify-between'>
+                <div className='flex gap-6 mb-8'>
+                <div>
+                    <img className='w-14 rounded' src={author_img} alt="" />
+                </div>
+                <div>
+                    <h3 className='text-2xl font-bold'>{author}</h3>
+                    <p className='text-base font-semibold text-[#11111199]'>{posted_date}</p>
+                </div>
+                </div>
+                <div>
+                    <span className='text-xl font-medium text-[#11111199]'>{reading_time} min read</span>
+                    <button
+                    onClick={() => handleAddBookmark(blog)} 
+                    className='ml-3 text-2xl text-red-500'>
+                        <FaRegBookmark></FaRegBookmark></button>
+                </div>
+                </div>
+            <h2 className="text-4xl mb-10 font-bold">{title}</h2>
+            <div className='flex gap-5 font-medium text-xl text-[#11111199] mb-8'>
+                <p>{hashtag}</p>
+                <p>{hashtag}</p>
+            </div>
+            </div>
     );
 };
 
 Blog.PropTypes = {
-    blog: PropTypes.object.isRequired
+    blog: PropTypes.object.isRequired,
+    handleAddBookmark: PropTypes.func
 }
 export default Blog;
